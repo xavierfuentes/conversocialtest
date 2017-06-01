6e6c6014b29d08cc4cd249a66b17225a63a5bae6
Xavi Fuentes <xavifuefer@gmail.com>
2017-06-01 15:55:51 +0100

    add redux to a create-react-app app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import configureStore from './data/store';
+import App from './components/App/App';
+
+import './index.css';
+
+const initialState = {};
+const store = configureStore(initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
---
+++ src/modules/ui/data/reducer.js
@@ -0,0 +1,15 @@
+import {} from './types';
+
+const initialState = {
+  //
+};
+
+const uiReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default uiReducer;
